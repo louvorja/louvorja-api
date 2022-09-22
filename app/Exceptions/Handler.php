5bac1b6f08ed025d60e2243eff4d5edf131e004c
maycorolbuche <85885740+maycorolbuche@users.noreply.github.com>
2022-09-22 17:22:07 -0300

    Ajustes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -49,6 +50,26 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        $rendered = parent::render($request, $exception);
+
+        if ($exception instanceof NotFoundHttpException) {
+            //You can do any change as per your requrement here for the not found exception
+            $message = $exception->getMessage() ? $exception->getMessage() : Response::$statusTexts[$rendered->getStatusCode()];
+            $exception = new NotFoundHttpException($message, $exception);
+        } elseif ($exception instanceof HttpException) {
+            $message = $exception->getMessage() ? $exception->getMessage() : Response::$statusTexts[$rendered->getStatusCode()];
+            $exception = new HttpException($rendered->getStatusCode(), $message);
+        } else {
+            $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+            $message = env('APP_DEBUG', false) ? $exception->getMessage() : Response::$statusTexts[$statusCode];
+            $exception = new HttpException($statusCode, $message);
+        }
+
+        // Resonse
+        return response()->json([
+            'status' => "error",
+            'message' => $exception->getMessage(),
+            'code' => $rendered->getStatusCode(),
+        ], $rendered->getStatusCode());
     }
 }
