7b59f84a547f80f78b673ac493cf5a4ea31dafa6
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-03-04 12:21:00 -0300

    Parametros

--- app/Http/Controllers/ParamsController.php
+++ app/Http/Controllers/ParamsController.php
@@ -21,6 +21,19 @@ class ParamsController extends Controller
         $es_delphi_version = Configs::get("es_delphi_version");
         $params["versaoES"] = $es_delphi_version;
 
+        $params["instalador"] = "setup\Output\LouvorJA_Instalador24.7.17233.35503.exe";
+        $params["instaladorES"] = "setup\Output\LoorJA_Instalador21.0.20.1.exe";
+        $params["form"] = "https://louvorja.com.br/contato/";
+        $params["formPT"] = "https://louvorja.com.br/contato/";
+        $params["formES"] = "https://louvorja.com.br/es/contacto/";
+        $params["coletaneas_online"] = "https://params.louvorja.com.br/baixa_coletaneas_web.php";
+        $params["embed_youtube"] = "https://www.youtube.com/embed/{videoID}";
+        $params["ftp"] = "https://params.louvorja.com.br/ftp.php";
+        $params["help"] = "https://louvorja.com.br/ajuda/";
+        $params["helpPT"] = "https://louvorja.com.br/ajuda/";
+        $params["helpES"] = "https://louvorja.com.br/es/ayuda/";
+        $params["logs_versao"] = "https://params.louvorja.com.br/logs_versao.php";
+
         if ($type == "env") {
             $text = "";
             foreach ($params as $key => $param) {
