426165d37a1770d583f9e11f409019e8b58f7e2e
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-03-13 21:51:13 -0300

    Ajuste

--- app/Http/Controllers/VersionLogController.php
+++ app/Http/Controllers/VersionLogController.php
@@ -24,7 +24,7 @@ class VersionLogController extends Controller
         $response = \Illuminate\Support\Facades\Http::get($url);
         $body = $response->getBody()->getContents();
 
-        dd($url, $body);
+        dd($url, $body->body);
 
         //return redirect($url);
     }
