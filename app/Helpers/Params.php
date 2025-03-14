d494211129a0dc7c32826392fef4ea5af8e33fc9
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-03-13 21:09:17 -0300

    Ajuste params

--- app/Helpers/Params.php
+++ app/Helpers/Params.php
@@ -26,21 +26,19 @@ class Params
         $params["es_version"] = $es_delphi_version;
 
         $params["conn_ftp"] = "https://api.louvorja.com.br/ftp?token=" . $token_ftp;
+        $params["version_log"] = "https://api.louvorja.com.br/version_log";
 
 
         /* A partir daqui, são todos parâmetros do Delphi */
         $params["instalador"] = "setup\Output\LouvorJA_Instalador" . $pt_delphi_version . ".exe"; // Diretório FTP de onde vai buscar o instalador
         $params["instaladorES"] = "setup\Output\LoorJA_Instalador" . $es_delphi_version . ".exe";
-        $params["form"] = "https://louvorja.com.br/contato/";
-        $params["formPT"] = "https://louvorja.com.br/contato/";
-        $params["formES"] = "https://louvorja.com.br/es/contacto/";
         $params["coletaneas_online"] = "https://params.louvorja.com.br/baixa_coletaneas_web.php";
         $params["embed_youtube"] = "https://www.youtube.com/embed/{videoID}";
         $params["ftp"] = "https://api.louvorja.com.br/ftp"; // REMOVER DEPOIS PARA MANTER A FORMA SEGURA (COM TOKEN)
         $params["help"] = "https://louvorja.com.br/ajuda/";
-        $params["helpPT"] = "https://louvorja.com.br/ajuda/";
-        $params["helpES"] = "https://louvorja.com.br/es/ayuda/";
-        $params["logs_versao"] = "https://params.louvorja.com.br/logs_versao.php";
+        $params["helpPT"] = $params["help"];
+        $params["helpES"] = $params["help"] . "?lang=es";
+        $params["logs_versao"] = $params["version_log"];
 
         return $params;
     }
